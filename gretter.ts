3d13251bcb9a5a33196f9692c42a0687ac2b02f0
Brotoo <brotoo-lu@hotmail.com>
2019-05-27 21:02:54 +0800

    hello

---
+++ gretter.ts
@@ -0,0 +1,11 @@
+function greeter(persion : string) {
+    return "Hello, " + persion;
+}
+
+let user : string = "ts";
+
+console.log(greeter(user));
+
+import * as ma from "./ma/ma"
+
+console.log(ma.a)
